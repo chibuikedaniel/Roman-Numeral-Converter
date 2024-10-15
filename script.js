0334348fa347b72943a0f167827552c40ce5d0b1
dracuzi <danielchibuike77@gmail.com>
2024-10-15 20:39:42 +0100

    fix bugs

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
 const convertBtn = document.getElementById('convert-btn');
-        const numberInput = document.getElementById('number');
-        const output = document.getElementById('output');
+const numberInput = document.getElementById('number');
+const output = document.getElementById('output');
 
-        convertBtn.addEventListener('click', () => {
-            const num = numberInput.value;
-            
-            if (!num) {
-                output.textContent = "Please enter a valid number";
-            } else if (num < 1) {
-                output.textContent = "Please enter a number greater than or equal to 1";
-            } else if (num >= 4000) {
-                output.textContent = "Please enter a number less than or equal to 3999";
-            } else {
-                output.textContent = convertToRoman(parseInt(num));
-            }
-        });
+convertBtn.addEventListener('click', () => {
+    const num = numberInput.value;
 
-        function convertToRoman(num) {
-            const romanNumerals = [
-                { value: 1000, numeral: 'M' },
-                { value: 900, numeral: 'CM' },
-                { value: 500, numeral: 'D' },
-                { value: 400, numeral: 'CD' },
-                { value: 100, numeral: 'C' },
-                { value: 90, numeral: 'XC' },
-                { value: 50, numeral: 'L' },
-                { value: 40, numeral: 'XL' },
-                { value: 10, numeral: 'X' },
-                { value: 9, numeral: 'IX' },
-                { value: 5, numeral: 'V' },
-                { value: 4, numeral: 'IV' },
-                { value: 1, numeral: 'I' }
-            ];
+    if (!num) {
+        output.textContent = "Please enter a valid number";
+    } else if (num < 1) {
+        output.textContent = "Please enter a number greater than or equal to 1";
+    } else {
+        output.textContent = convertToRoman(parseInt(num));
+    }
+});
 
-            let result = '';
-            for (let i = 0; i < romanNumerals.length; i++) {
-                while (num >= romanNumerals[i].value) {
-                    result += romanNumerals[i].numeral;
-                    num -= romanNumerals[i].value;
-                }
-            }
-            return result;
+function convertToRoman(num) {
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' }
+    ];
+
+    let result = '';
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i].value) {
+            result += romanNumerals[i].numeral;
+            num -= romanNumerals[i].value;
         }
+    }
+    return result;
+}
